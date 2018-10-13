b3d8622aaf38d7cf477b795d0af895d0b475c046
zhongpeng <964176307@qq.com>
2018-10-13 08:39:57 +0800

    Fix can't run to android

--- android/app/src/main/java/com/reduxforreactnativedemo/MainActivity.java
+++ android/app/src/main/java/com/reduxforreactnativedemo/MainActivity.java
@@ -1,16 +1,7 @@
 package com.reduxforreactnativedemo;
 
-import com.facebook.react.ReactActivity;
 import com.reactnativenavigation.controllers.SplashActivity;
 
 public class MainActivity extends SplashActivity {
 
-    /**
-     * Returns the name of the main component registered from JavaScript.
-     * This is used to schedule rendering of the component.
-     */
-    @Override
-    protected String getMainComponentName() {
-        return "ReduxForReactNativeDemo";
-    }
 }
--- android/app/src/main/java/com/reduxforreactnativedemo/MainApplication.java
+++ android/app/src/main/java/com/reduxforreactnativedemo/MainApplication.java
@@ -1,43 +1,29 @@
 package com.reduxforreactnativedemo;
 
-import android.app.Application;
-
-import com.facebook.react.ReactApplication;
-import com.reactnativenavigation.NavigationReactPackage;
-import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
-import com.facebook.soloader.SoLoader;
 
 import java.util.Arrays;
 import java.util.List;
 import com.reactnativenavigation.NavigationApplication;
 
- public class MainApplication extends NavigationApplication {
+public class MainApplication extends NavigationApplication {
+    @Override
+    public boolean isDebug() {
+        return BuildConfig.DEBUG;
+    }
 
-     @Override
-     public boolean isDebug() {
-         // Make sure you are using BuildConfig from your own application
-         return BuildConfig.DEBUG;
-     }
 
-     protected List<ReactPackage> getPackages() {
-         // Add additional packages you require here
-         // No need to add RnnPackage and MainReactPackage
-         return Arrays.<ReactPackage>asList(
-             new MainReactPackage(),
-             new NavigationReactPackage()
-         );
-     }
+    @Override
+    public List<ReactPackage> createAdditionalReactPackages() {
+        return Arrays.<ReactPackage>asList(
+              //
+        );
+    }
 
-     @Override
-     public List<ReactPackage> createAdditionalReactPackages() {
-         return getPackages();
-     }
+    @Override
+    public String getJSMainModuleName() {
+        return "index";
+    }
+}
 
-     @Override
-     public String getJSMainModuleName() {
-         return "index";
-     }
- }
 
